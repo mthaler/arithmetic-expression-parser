95870829d63cb01be9b850a0a4279a8ed355c7ad
Michael Thaler <michael.thaler@dlr.de>
2021-01-01 13:54:39 +0100

    More work on creating an AST.

--- src/main/kotlin/com/mthaler/parser/ast/Parsers.kt
+++ src/main/kotlin/com/mthaler/parser/ast/Parsers.kt
@@ -2,12 +2,21 @@ package com.mthaler.parser.ast
 
 import com.mthaler.parser.Result
 import com.mthaler.parser.map
+import com.mthaler.parser.or
 import com.mthaler.parser.tokens.number as tnumber
 import com.mthaler.parser.tokens.charLiteral
 
+typealias TerminalParser = (String) -> Result<Terminal>
+
 typealias ExpressionParser = (String) -> Result<Expression>
 
-val number = ::tnumber.map { Terminal.Number(it.toDouble()) }
+// terminals
 
-val plus = charLiteral('+').map { Terminal.BinaryOperator(it) }
-val minus = charLiteral('-').map { Terminal.BinaryOperator(it) }
+val number: TerminalParser = ::tnumber.map { Terminal.Number(it.toDouble()) }
+
+val plus: TerminalParser = charLiteral('+').map { Terminal.BinaryOperator(it) }
+val minus: TerminalParser = charLiteral('-').map { Terminal.BinaryOperator(it) }
+val times: TerminalParser = charLiteral('*').map { Terminal.BinaryOperator(it) }
+val div: TerminalParser = charLiteral('/').map { Terminal.BinaryOperator(it) }
+
+val binaryOperator = plus or minus or times or div
--- src/main/kotlin/com/mthaler/parser/ast/Terminal.kt
+++ src/main/kotlin/com/mthaler/parser/ast/Terminal.kt
@@ -3,6 +3,6 @@ package com.mthaler.parser.ast
 sealed class Terminal {
 
     data class Number(val value: Double) : Terminal()
-    data class BinaryOperator(val operator: String) : Terminal()
+    data class BinaryOperator(val operator: String) : Terminal(), com.mthaler.parser.ast.BinaryOperator
 }
 
