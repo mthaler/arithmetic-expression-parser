09005e4a7d044bf64beef6ebe09f607bac7f86ff
Michael Thaler <michael.thaler@dlr.de>
2021-01-01 09:56:18 +0100

    Added string literal parser.

--- src/main/kotlin/com/mthaler/parser/Parsers.kt
+++ src/main/kotlin/com/mthaler/parser/Parsers.kt
@@ -8,3 +8,10 @@ fun charLiteral(c: Char): Parser<Char> = { input ->
     else
         Result.Err("a '$c'", input)
 }
+
+fun stringLiteral(s: String): Parser<String> = { input ->
+    if (input.startsWith(s))
+        Result.OK(s, input.substring(s.length))
+    else
+        Result.Err("'$s'", input)
+}
--- src/test/kotlin/com/mthaler/parser/ParsersTest.kt
+++ src/test/kotlin/com/mthaler/parser/ParsersTest.kt
@@ -10,4 +10,10 @@ class ParsersTest: StringSpec({
         p(".foo") shouldBe Result.OK('.', "foo")
         p("foo") shouldBe Result.Err("a '.'", "foo")
     }
+
+    "stringLiteral" {
+        val p = stringLiteral("foo")
+        p("foobar") shouldBe Result.OK("foo", "bar")
+        p("boofar") shouldBe Result.Err("'foo'", "boofar")
+    }
 })
