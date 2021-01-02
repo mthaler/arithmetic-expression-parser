e3251cedd5851c5a418a80869f723b934b3f1d4b
Michael Thaler <michael.thaler@dlr.de>
2021-01-02 10:04:57 +0100

    Added Expr.UnaryOp.

--- src/main/kotlin/com/mthaler/parser/arithmetic/Expr.kt
+++ src/main/kotlin/com/mthaler/parser/arithmetic/Expr.kt
@@ -3,5 +3,6 @@ package com.mthaler.parser.arithmetic
 sealed class Expr {
 
     data class Number(val number: Double): Expr()
+    data class UnaryOp(val operand: Expr, val operator: String): Expr()
     data class BinOp(val operand1: Expr, val operand2: Expr, val operator: String): Expr()
 }
