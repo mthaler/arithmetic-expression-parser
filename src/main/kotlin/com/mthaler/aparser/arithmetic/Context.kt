4dcb4031c7859cb1ea156b19b5dc7c2fdf258b02
Michael Thaler <michael.thaler@dlr.de>
2021-05-21 08:55:45 +0200

    Updated Context class.

--- src/main/kotlin/com/mthaler/aparser/arithmetic/Context.kt
+++ src/main/kotlin/com/mthaler/aparser/arithmetic/Context.kt
@@ -3,9 +3,9 @@ package com.mthaler.aparser.arithmetic
 /**
  * Context for evaluating expressions
  */
-data class Context(val globalVars: Map<String, Double>) {
+data class Context(val trigonometricUnit: TrigonometricUnit, val globalVars: Map<String, Double>) {
 
     companion object {
-        val Empty = Context(emptyMap())
+        val Empty = Context(TrigonometricUnit.Rad, emptyMap())
     }
 }
