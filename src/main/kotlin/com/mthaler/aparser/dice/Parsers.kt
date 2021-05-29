6b18413be5a56e367a1b7a51ec1b312a1ad59fea
Michael Thaler <michael.thaler@dlr.de>
2021-05-29 13:42:31 +0200

    Finished implementing dice.Expression.

--- src/main/kotlin/com/mthaler/aparser/dice/Parsers.kt
+++ src/main/kotlin/com/mthaler/aparser/dice/Parsers.kt
@@ -6,7 +6,7 @@ import com.mthaler.aparser.common.*
 import com.mthaler.aparser.tokens.charLiteral
 import com.mthaler.aparser.tokens.digits
 
-val integer: Parser<Expr>  = ws(digits).map { Expr.Number(it.toInt()) }
+val number: Parser<Expr>  = ws(digits).map { Expr.Number(it.toInt()) }
 
 val die: Parser<Expr> = ws(optional(digits) and charLiteral('d') and digits).map {
     val optionalCount = it.first.first
@@ -16,24 +16,36 @@ val die: Parser<Expr> = ws(optional(digits) and charLiteral('d') and digits).map
 
 object Expression: RecursiveParser<Expr>() {
 
-    val group: Parser<Expr> = (lpar and this and rpar).map { it.middle() }
+    init {
+        val group: Parser<Expr> = (lpar and this and rpar).map { it.middle() }
 
-    val factor: Parser<Expr> = (neg and integer).map { Expr.UnaryOp(it.second, it.first) as Expr } or integer
+        val factor: Parser<Expr> = (neg and number).map { Expr.UnaryOp(it.second, it.first) as Expr } or number
 
-    val operand: Parser<Expr> = die or factor or group
+        val operand: Parser<Expr> = die or factor or group
 
-    val power: Parser<Expr> = (operand and zeroOrMore(exp and operand)).map { p ->
-        if (p.second.isEmpty()) {
-            p.first
-        } else {
-            val rest = p.second
-            var e = p.first
-            val result = arrayListOf<Pair<String, Expr>>()
-            for (item in rest) {
-                result.add(Pair(item.first, e))
-                e = item.second
+        val power: Parser<Expr> = (operand and zeroOrMore(exp and operand)).map { p ->
+            if (p.second.isEmpty()) {
+                p.first
+            } else {
+                val rest = p.second
+                var e = p.first
+                val result = arrayListOf<Pair<String, Expr>>()
+                for (item in rest) {
+                    result.add(Pair(item.first, e))
+                    e = item.second
+                }
+                result.foldRight(e) { item, expr -> Expr.BinOp(item.second, expr, item.first) }
             }
-            result.foldRight(e) { item, expr -> Expr.BinOp(item.second, expr, item.first) }
         }
+
+        val term: Parser<Expr> = (power and zeroOrMore((times or div) and power)).map { p ->
+            p.second.fold(p.first) { expr, item -> Expr.BinOp(expr, item.second, item.first) }
+        }
+
+        val expr: Parser<Expr> = (term and zeroOrMore((plus or minus) and term)).map { p ->
+            p.second.fold(p.first) { expr, item -> Expr.BinOp(expr, item.second, item.first) }
+        }
+
+        this.parser = expr or die or number
     }
 }
