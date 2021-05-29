02e797b9f5d7866faf8ca687d144ff51743ea6d4
Michael Thaler <michael.thaler@dlr.de>
2021-05-29 15:36:53 +0200

    More work on eval for dice.

--- src/main/kotlin/com/mthaler/aparser/dice/Eval.kt
+++ src/main/kotlin/com/mthaler/aparser/dice/Eval.kt
@@ -3,6 +3,7 @@ package com.mthaler.aparser.dice
 import com.mthaler.aparser.util.ParseException
 import com.mthaler.aparser.util.Result
 import com.mthaler.aparser.util.Try
+import java.lang.IllegalArgumentException
 
 /*
  * Evaluates the given expression. This method can throw an exception if e.g. an undefined function name is used
@@ -22,5 +23,16 @@ fun Result<Expr>.tryEval(rng: RNG = RNG.Default): Try<Int> = when(this) {
  */
 private fun eval(expr: Expr, rng: RNG): Int = when(expr) {
     is Expr.Number -> expr.number
-    else -> TODO()
+    is Expr.Die -> TODO()
+    is Expr.UnaryOp -> when(val op = expr.operator) {
+        "-" -> -eval(expr.operand, rng)
+        else -> throw IllegalArgumentException("Unknown operator: $op")
+    }
+    is Expr.BinOp -> when(val op = expr.operator) {
+        "+" -> eval(expr.operand1, rng) + eval(expr.operand2, rng)
+        "-" -> eval(expr.operand1, rng) - eval(expr.operand2, rng)
+        "*" -> eval(expr.operand1, rng) * eval(expr.operand2, rng)
+        "/" -> eval(expr.operand1, rng) / eval(expr.operand2, rng)
+        else -> throw IllegalArgumentException("Unknown operator: $op")
+    }
 }
