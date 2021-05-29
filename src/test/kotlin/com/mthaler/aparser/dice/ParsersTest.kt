9996dd8360f8db3bd36e7cf7d1863036e5d6f697
Michael Thaler <michael.thaler@dlr.de>
2021-05-29 13:58:49 +0200

    Added expression test to ParsersTest.

--- src/test/kotlin/com/mthaler/aparser/dice/ParsersTest.kt
+++ src/test/kotlin/com/mthaler/aparser/dice/ParsersTest.kt
@@ -21,4 +21,15 @@ class ParsersTest: StringSpec({
         number("foo") shouldBe Result.Err("digits", "foo")
         number("foo123") shouldBe Result.Err("digits", "foo123")
     }
+
+    "expression" {
+        val e = Expression
+        e("3") shouldBe Result.OK(Expr.Number(3), "")
+        e("-3") shouldBe Result.OK(Expr.UnaryOp(Expr.Number(3), "-"), "")
+        e("3+4") shouldBe Result.OK(Expr.BinOp(Expr.Number(3), Expr.Number(4), "+"), "")
+        e("3 + 4") shouldBe Result.OK(Expr.BinOp(Expr.Number(3), Expr.Number(4), "+"), "")
+        e("3 + (4 + 5)") shouldBe Result.OK(Expr.BinOp(Expr.Number(3), Expr.BinOp(Expr.Number(4),Expr.Number(5), "+"), "+"), "")
+        e("(3 + 4) + 5") shouldBe Result.OK(Expr.BinOp(Expr.BinOp(Expr.Number(3), Expr.Number(4), "+"), Expr.Number(5), "+"), "")
+        e("3 + 4 + 5") shouldBe Result.OK(Expr.BinOp(Expr.BinOp(Expr.Number(3), Expr.Number(4), "+"), Expr.Number(5), "+"), "")
+    }
 })
