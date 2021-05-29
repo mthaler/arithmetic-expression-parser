065f3d8c6fad25be153a272f429a442ed5038b82
Michael Thaler <michael.thaler@dlr.de>
2021-05-29 14:41:07 +0200

    Added default implemtation for RNG.

--- src/main/kotlin/com/mthaler/aparser/dice/Eval.kt
+++ src/main/kotlin/com/mthaler/aparser/dice/Eval.kt
@@ -7,20 +7,20 @@ import com.mthaler.aparser.util.Try
 /*
  * Evaluates the given expression. This method can throw an exception if e.g. an undefined function name is used
  */
-fun Result<Expr>.eval(): Result<Int> = map { eval(it) }
+fun Result<Expr>.eval(rng: RNG = RNG.Default): Result<Int> = map { eval(it, rng) }
 
 /**
  * Evaluates the given expression. This method will return a try, it will not throw an exception if the expression cannot be evaluated
  */
-fun Result<Expr>.tryEval(): Try<Int> = when(this) {
-    is Result.OK -> Try { eval(this.value) }
+fun Result<Expr>.tryEval(rng: RNG = RNG.Default): Try<Int> = when(this) {
+    is Result.OK -> Try { eval(this.value, rng) }
     is Result.Err -> Try.Failure(ParseException(this.expected, this.input))
 }
 
 /**
  * Evaluates the given expression
  */
-private fun eval(expr: Expr): Int = when(expr) {
+private fun eval(expr: Expr, rng: RNG): Int = when(expr) {
     is Expr.Number -> expr.number
     else -> TODO()
 }
--- src/main/kotlin/com/mthaler/aparser/dice/RNG.kt
+++ src/main/kotlin/com/mthaler/aparser/dice/RNG.kt
@@ -1,6 +1,17 @@
 package com.mthaler.aparser.dice
 
+import kotlin.random.Random
+
 interface RNG {
 
-    fun nextInt(from: Int, until: Int)
+    fun nextInt(from: Int, until: Int): Int
+
+    companion object {
+
+        val Default = object : RNG {
+            val random = Random.Default
+
+            override fun nextInt(from: Int, until: Int): Int = random.nextInt(from, until)
+        }
+    }
 }
