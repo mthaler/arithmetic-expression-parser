c9b320fd848e7e3afc3cf76a791fdab7df694a27
Michael Thaler <michael.thaler@dlr.de>
2021-05-29 15:21:48 +0200

    Updated RNG.

--- src/main/kotlin/com/mthaler/aparser/dice/RNG.kt
+++ src/main/kotlin/com/mthaler/aparser/dice/RNG.kt
@@ -4,14 +4,14 @@ import kotlin.random.Random
 
 interface RNG {
 
-    fun nextInt(from: Int, until: Int): Int
+    fun nextInt(until: Int): Int
 
     companion object {
 
         val Default = object : RNG {
             val random = Random.Default
 
-            override fun nextInt(from: Int, until: Int): Int = random.nextInt(from, until)
+            override fun nextInt(until: Int): Int = random.nextInt(until)
         }
     }
 }
--- src/test/kotlin/com/mthaler/aparser/dice/TestRNG.kt
+++ src/test/kotlin/com/mthaler/aparser/dice/TestRNG.kt
@@ -4,11 +4,11 @@ class TestRNG(val values: List<Int>): RNG {
 
     var count = 0
 
-    override fun nextInt(from: Int, until: Int): Int {
+    override fun nextInt(until: Int): Int {
         val value = values[count % values.size]
         count++
-        if (value < from) {
-            return from
+        if (value < 0) {
+            return 0
         } else if(value >= until) {
             return until - 1
         } else {
